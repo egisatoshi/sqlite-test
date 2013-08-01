ab9dad206f3e8ad73ae1a3727b7fbc12a7f7b1b8
Satoshi Egi <egisatoshi@gmail.com>
2013-08-01 14:41:16 +0900

    update

--- test.sql
+++ test.sql
@@ -2,6 +2,7 @@
 
 explain select * from twitter_user where id = 20;
 explain select * from follow where id = 20;
+explain select * from follow where id < 20;
 explain select * from follow where from_id = 20;
 
 explain select * from twitter_user as twitter_user1, follow, twitter_user as twitter_user2
@@ -9,9 +10,7 @@ explain select * from twitter_user as twitter_user1, follow, twitter_user as twi
     and follow.to_id = twitter_user2.id
     and twitter_user1.id = 20;
 
-/*
-select * from twitter_user as twitter_user1, follow, twitter_user as twitter_user2
+explain select twitter_user1.id, twitter_user2.id from twitter_user as twitter_user1, follow, twitter_user as twitter_user2
   where twitter_user1.id = follow.from_id
     and follow.to_id = twitter_user2.id
     and twitter_user1.id = 20;
-*/
